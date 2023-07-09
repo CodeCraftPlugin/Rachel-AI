bdd232f0779af6300e74482002f5b1c09c09898f
CodeCraft <86466022+CodeCraftPlugin@users.noreply.github.com>
2023-07-09 17:53:22 +0530

    FIXED A MAJOR PROBLEM IN THE BUILD FILE

--- build.bat
+++ build.bat
@@ -1,17 +1,12 @@
 @echo off
 setlocal
-set "build_exe=%~1"
+set build_exe=%1
 set justmakeexe = %2
-:build_exe
-python setup.py build
-:setup-ai
-py -m venv env
-call "env\Scripts\activate.bat"
-pip install -r requirements.txt
+
 if "%build_exe%"=="true" (
     call :build_exe
     call :setup-ai
-    
+    goto :eof   
 )
 if "%justmakeexe%"=="true" (
     call :build_exe
@@ -22,6 +17,15 @@ echo Setting up the file...
 call :setup-ai
 echo Done!
 call py main.py
-
 echo Process completed.
 pause
+goto :eof
+
+:build_exe
+python setup.py build
+goto :eof
+:setup-ai
+py -m venv env
+call "env\Scripts\activate.bat"
+pip install -r requirements.txt
+goto :eof
